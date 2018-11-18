115441085723f662fbb165b223717b8b9062a778
Rustem Alkapov <ralkapov@gmail.com>
2018-11-19 01:55:51 +0500

    add task 3

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, pqconnection, sqldb, db, FileUtil, Forms, Controls,
-  Graphics, Dialogs, DBGrids;
+  Graphics, Dialogs, DBGrids, ComCtrls;
 
 type
 
@@ -15,9 +15,18 @@ type
   TForm1 = class(TForm)
     DataSource1: TDataSource;
     DBGrid1: TDBGrid;
+    PageControl1: TPageControl;
     PQConnection1: TPQConnection;
     SQLQuery1: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
+    TabSheet5: TTabSheet;
+    TabSheet6: TTabSheet;
+    TabSheet7: TTabSheet;
+    procedure PageControl1Change(Sender: TObject);
   private
 
   public
@@ -30,6 +39,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.PageControl1Change(Sender: TObject);
+begin
+
+end;
 
 end.
 
--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, pqconnection, sqldb, db, FileUtil, Forms, Controls,
-  Graphics, Dialogs, DBGrids;
+  Graphics, Dialogs, DBGrids, ComCtrls;
 
 type
 
@@ -15,9 +15,21 @@ type
   TForm1 = class(TForm)
     DataSource1: TDataSource;
     DBGrid1: TDBGrid;
+    PageControl1: TPageControl;
     PQConnection1: TPQConnection;
     SQLQuery1: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
+    TabSheetCities: TTabSheet;
+    TabSheetSuppliers: TTabSheet;
+    TabSheetContracts: TTabSheet;
+    TabSheetDrugClasses: TTabSheet;
+    TabSheetDrugPrices: TTabSheet;
+    TabSheetDrugRegistry: TTabSheet;
+    TabSheetDrugs: TTabSheet;
+    TabSheetOrders: TTabSheet;
+    TabSheetProducts: TTabSheet;
+    TabSheetPurchases: TTabSheet;
+    procedure PageControl1Change(Sender: TObject);
   private
 
   public
@@ -30,6 +42,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.PageControl1Change(Sender: TObject);
+begin
+
+end;
 
 end.
 
